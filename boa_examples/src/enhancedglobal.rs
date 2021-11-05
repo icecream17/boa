1b6a95a12d9d03d16f23f4083fd210a21e25f124
RageKnify <RageKnify@gmail.com>
2021-11-06 00:57:03 +0100

    style: make examples respect rust clippy

--- boa_examples/src/enhancedglobal.rs
+++ boa_examples/src/enhancedglobal.rs
@@ -5,41 +5,49 @@ pub fn run() {
     let js_file_path = "./scripts/enhancedglobal.js";
     let buffer = read_to_string(js_file_path);
 
-    if buffer.is_err() {
-        println!("Error: {}", buffer.unwrap_err());
-        return;
+    match buffer {
+        Err(error) => {
+            println!("Error: {}", error);
+        }
+        Ok(buffer) => {
+            // We need to create a Javascript context.
+            let mut ctx = Context::new();
+
+            // We can register a global property
+            ctx.register_global_property(
+                "customstring",
+                "Hello! I am a custom global variable",
+                Attribute::all(),
+            );
+
+            if let Err(error) = ctx.register_global_closure("rusty_hello", 0, |_, args, _| {
+                let arg = args.get(0).unwrap();
+                let val = format!(
+                    "Hello from Rust! You passed {}",
+                    arg.as_string().unwrap().as_str()
+                );
+
+                Ok(val.into())
+            }) {
+                println!("Error: {}", error.display().to_string());
+            };
+
+            if let Err(error) = ctx.register_global_closure("add", 0, |_, args, _| {
+                let arg = args.get(0).unwrap();
+                let val = format!(
+                    "Hello from Rust! You passed {}",
+                    arg.as_string().unwrap().as_str()
+                );
+
+                Ok(val.into())
+            }) {
+                println!("Error: {}", error.display().to_string());
+            };
+
+            //Loading, parsing and executing the JS code from the source file
+            if let Err(error) = ctx.eval(&buffer) {
+                println!("Error: {}", error.display().to_string());
+            }
+        }
     }
-
-    // We need to create a Javascript context.
-    let mut ctx = Context::new();
-
-    // We can register a global property
-    ctx.register_global_property(
-        "customstring",
-        "Hello! I am a custom global variable",
-        Attribute::all(),
-    );
-
-    ctx.register_global_closure("rusty_hello", 0, |_, args, _| {
-        let arg = args.get(0).unwrap();
-        let val = format!(
-            "Hello from Rust! You passed {}",
-            arg.as_string().unwrap().as_str()
-        );
-
-        Ok(val.into())
-    });
-
-    ctx.register_global_closure("add", 0, |_, args, _| {
-        let arg = args.get(0).unwrap();
-        let val = format!(
-            "Hello from Rust! You passed {}",
-            arg.as_string().unwrap().as_str()
-        );
-
-        Ok(val.into())
-    });
-
-    //Loading, parsing and executing the JS code from the source file
-    ctx.eval(&buffer.unwrap());
 }
